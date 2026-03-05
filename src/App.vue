b6e19c328ad6b320a0011c8b4965cb0f46bb6e47
istiuak <istiuak.0@gmail.com>
2026-03-05 15:39:20 +0600

    add tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
-<script setup lang="ts"> 
- 
- </script>
-<template>
+<script setup lang="ts">
 
+</script>
+<template>
+  <p class="bg-red-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, quos praesentium, aut odit rerum
+    quidem id ipsam dolorum cum dolores sunt reprehenderit expedita alias illo eum inventore tempore rem optio?</p>
 </template>
